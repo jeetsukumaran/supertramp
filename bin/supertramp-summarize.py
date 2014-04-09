b80d387ad6cdb8b29f38220a754f83a0d96f0b14
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-04-09 10:28:20 -0400

    Working on summarization

--- bin/supertramp-summarize.py
+++ bin/supertramp-summarize.py
@@ -6,13 +6,13 @@ import argparse
 import json
 import collections
 import dendropy
-
+from dendropy import treecalc
 if dendropy.__version__.startswith("4"):
     _get_taxa = lambda x: x.taxon_namespace
 else:
     _get_taxa = lambda x: x.taxon_set
 
-class TreeColorizer(object):
+class TreeProcessor(object):
 
     def __init__(self):
 
@@ -45,6 +45,76 @@ class TreeColorizer(object):
                 trees.write_to_stream(outf, "nexus")
             except AttributeError:
                 self.write_nexus(trees, outf)
+
+    def get_mean_patristic_distance(self, pdm, nodes):
+        weighted_dist = 0.0
+        unweighted_dist = 0.0
+        ncomps = 0
+        for idx1, nd1 in enumerate(nodes[:-1]):
+            for nd2 in nodes[idx1+1:]:
+                assert nd1.taxon
+                assert nd2.taxon
+                weighted_dist += pdm(nd1.taxon, nd2.taxon)
+                unweighted_dist += pdm.path_edge_count(nd1.taxon, nd2.taxon)
+                ncomps += 1
+        return weighted_dist/ncomps, unweighted_dist/ncomps
+
+    def process_trees(self, trees, trees_outf):
+        self.encode_taxa(_get_taxa(trees))
+        for tree in trees:
+            num_tips = 0
+            total_length = 0.0
+            nodes_by_island = collections.defaultdict(list)
+            nodes_by_habitat = collections.defaultdict(list)
+            for nd in tree:
+                # colorize
+                if nd.taxon is None and nd.label is None:
+                    continue
+                if nd.taxon and nd.taxon.habitat_color is not None:
+                    nd.annotations.add_new("!color", nd.taxon.habitat_color)
+                elif nd.label is not None:
+                    self.encode_labeled_item(nd, annotate_habitat_color=True)
+                # stats
+                num_tips += 1
+                total_length += nd.edge.length
+                if nd.is_leaf():
+                    island_code = nd.taxon.island_code
+                    for idx, i in enumerate(island_code):
+                        island_idx = len(island_code) - idx
+                        if i == "1":
+                            nodes_by_island[island_idx].append(nd)
+                    habitat_code = nd.taxon.habitat_code
+                    for idx, i in enumerate(habitat_code):
+                        habitat_idx = len(habitat_code) - idx
+                        if i == "1":
+                            nodes_by_habitat[habitat_idx].append(nd)
+            pdm = treecalc.PatristicDistanceMatrix(tree=tree)
+            tree.stats = {}
+            tree.stats["size"] = num_tips
+            tree.stats["length"] = total_length
+
+            weighted_dist_total = 0.0
+            unweighted_dist_total = 0.0
+            nitems = 0
+            habitat_keys = sorted(nodes_by_habitat.keys())
+            for key in habitat_keys:
+                weighted, unweighted = self.get_mean_patristic_distance(pdm, nodes_by_habitat[key])
+                weighted = weighted/total_length
+                unweighted = unweighted/num_tips
+                tree.stats["habitat.{}.pdist.weighted".format(key+1)] = weighted
+                tree.stats["habitat.{}.pdist.unweighted".format(key+1)] = unweighted
+                weighted_dist_total += weighted
+                unweighted_dist_total += unweighted
+                nitems += 1
+            tree.stats["habitat.mean.pdist.weighted"] = weighted_dist_total / nitems
+            tree.stats["habitat.mean.pdist.unweighted"] = unweighted_dist_total / nitems
+
+        if trees_outf is not None:
+            try:
+                trees.write_to_stream(trees_outf, "nexus")
+            except AttributeError:
+                self.write_nexus(trees, trees_outf)
+        return trees
 
     def encode_labeled_item(self,
             t,
@@ -102,7 +172,7 @@ def main():
     args = parser.parse_args()
     args.quiet = False
 
-    tree_colorizer = TreeColorizer()
+    tree_processor = TreeProcessor()
     for source_path in args.source_paths:
         source_dir = os.path.abspath(os.path.expanduser(os.path.expandvars(source_path)))
         run_manifest_path = os.path.join(source_dir, "run-manifest.json")
@@ -121,11 +191,9 @@ def main():
                     "newick")
             colorized_trees_filepath = args.output_prefix + ".{}.trees".format(key)
             with open(colorized_trees_filepath, "w") as trees_outf:
-                tree_colorizer.colorize_trees(trees, trees_outf)
-            # for tree_idx, tree in enumerate(trees):
-            #     if not args.quiet:
-            #         sys.stderr.write("Processing tree {} of {} in job {} of {}\n".format(tree_idx+1, len(trees), key_idx+1, len(jobs), key))
-
+                summary_stats = tree_processor.process_trees(trees, trees_outf=trees_outf)
+    # summary_stats_fpath = args.output_prefix + ".summary.txt"
+    # with open(summary_stats_fpath, "w") as summary_outf:
 
 if __name__ == "__main__":
     main()
