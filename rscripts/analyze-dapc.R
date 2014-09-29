88f5002b307893066040e5f3df3591fa392e8546
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-09-29 01:00:09 -0400

    Update r script

--- rscripts/analyze-dapc.R
+++ rscripts/analyze-dapc.R
@@ -1,22 +1,51 @@
 library(adegenet)
-summary.df = read.table("processed/summary.txt", header=T)
-summary.df = na.omit(summary.df)
-groups = summary.df$dispersal.model
-cols.to.drop <- c(
-                  "dispersal.model",
-                  "birth.rate",
-                  "death.rate",
-                  "dispersal.rate",
-                  "niche.evolution.prob",
-                  "edges",
-                  "est.birth.rate",
-                  "length",
-                  "size"
-                  )
 
-predictors = summary.df[,!(names(summary.df) %in% cols.to.drop)]
+SIM.PARAMS <- c(
+               "dispersal.model",
+               "birth.rate",
+               "death.rate",
+               "dispersal.rate",
+               "niche.evolution.prob"
+               )
+RESULT.METADATA <- c(
+                    "edges",
+                    "est.birth.rate",
+                    "length",
+                    "size"
+                    )
+COLS.TO.DROP <- c(SIM.PARAMS, RESULT.METADATA)
 
-analyze.dapc = function(n.pc, n.da) {
+summary.df <- read.table("processed/summary.txt", header<-T)
+# summary.b1 = summary.df[summary.df$birth.rate==0.03,]
+# dispersal.model :  1 2
+# birth.rate :  0.003 0.03
+# death.rate :  0
+# dispersal.rate :  0.03 0.15 0.3
+# niche.evolution.prob :  0.03 0.15 0.5 1
+# > qqq = summary.df[summary.df$birth.rate==0.03 & summary.df$dispersal.rate==0.3 & summary.df$niche.evolution.prob==1,]
+# > x = create.data(qqq)
+# > predictors = x$predictors
+# > groups = x$groups
+
+data.regimes <- function(summary.df) {
+    for (sim.param in SIM.PARAMS) {
+        cat(sim.param, ": ", sort(unique(summary.df[,sim.param])), "\n")
+    }
+}
+
+
+create.groups.and.predictors = function(source.df) {
+    source.df = na.omit(source.df)
+    groups = source.df$dispersal.model
+    predictors = source.df[,!(names(source.df) %in% COLS.TO.DROP)]
+    rv = list(
+        groups=groups,
+        predictors=predictors
+        )
+    rv
+}
+
+analyze.dapc = function(predictors, groups, n.pc, n.da) {
     dapc.result = dapc(predictors, groups, n.pc=n.pc, n.da=n.da)
     var.contr = data.frame(var=rownames(dapc.result$var.contr),
                         LD1=as.vector(dapc.result$var.contr)
@@ -43,12 +72,12 @@ analyze.dapc = function(n.pc, n.da) {
     rv
 }
 
-assess.vars = function() {
+assess.vars = function(predictors, groups) {
     result = data.frame()
     for (n.pc in 2:ncol(predictors)) {
         n.da = 10
-        for (n.da in 1:ncol(predictors)) {
-            x = analyze.dapc(n.pc, n.da)
+        # for (n.da in 1:ncol(predictors)) {
+            x = analyze.dapc(predictors, groups, n.pc, n.da)
             cat(paste(n.pc,
                         n.da,
                         x$mean.pp.for.correct.model,
@@ -63,9 +92,68 @@ assess.vars = function() {
                         mean.prop.correct.model.preferred=x$mean.prop.correct.model.preferred
                         )
             result = rbind(result, subresult)
-        }
+        # }
     }
     result
     #  z.pp = result[order(-result$mean.pp.for.correct.model,result$n.pc,result$n.da),]
     #  z.prop = result[order(-result$mean.prop.correct.model.preferred,result$n.pc,result$n.da),]
+}
+
+create.data.for.regime <- function(
+                   filter.for.birth.rate=NULL,
+                   filter.for.dispersal.rate=NULL,
+                   filter.for.niche.evolution.prob=NULL) {
+    summary.df.copy = summary.df
+    if (!is.null(filter.for.birth.rate)) {
+        summary.df.copy = subset(summary.df.copy, birth.rate==filter.for.birth.rate)
+    }
+    if (!is.null(filter.for.niche.evolution.prob)) {
+        summary.df.copy = subset(summary.df.copy, niche.evolution.prob==filter.for.niche.evolution.prob)
+    }
+    if (!is.null(filter.for.dispersal.rate)) {
+        summary.df.copy = subset(summary.df.copy, dispersal.rate==filter.for.dispersal.rate)
+    }
+    summary.df.copy
+}
+
+create.groups.and.predictors.for.regime = function(
+                                                   filter.for.birth.rate=NULL,
+                                                   filter.for.dispersal.rate=NULL,
+                                                   filter.for.niche.evolution.prob=NULL) {
+    regime.summary = create.data.for.regime(
+                                            filter.for.birth.rate=filter.for.birth.rate,
+                                            filter.for.dispersal.rate=filter.for.dispersal.rate,
+                                            filter.for.niche.evolution.prob=filter.for.niche.evolution.prob)
+    x = create.groups.and.predictors(regime.summary)
+    x
+}
+
+assess.vars.for.regime = function(
+                   filter.for.birth.rate=NULL,
+                   filter.for.dispersal.rate=NULL,
+                   filter.for.niche.evolution.prob=NULL) {
+    x = create.groups.and.predictors.for.regime(
+                   filter.for.birth.rate=filter.for.birth.rate,
+                   filter.for.dispersal.rate=filter.for.dispersal.rate,
+                   filter.for.niche.evolution.prob=filter.for.niche.evolution.prob)
+    groups = x$groups
+    predictors = x$predictors
+    rv = assess.vars(predictors, groups)
+    rv
+}
+
+analyze.dapc.for.regime = function(
+                   n.pc,
+                   n.da,
+                   filter.for.birth.rate=NULL,
+                   filter.for.dispersal.rate=NULL,
+                   filter.for.niche.evolution.prob=NULL) {
+    x = create.groups.and.predictors.for.regime(
+                   filter.for.birth.rate=filter.for.birth.rate,
+                   filter.for.dispersal.rate=filter.for.dispersal.rate,
+                   filter.for.niche.evolution.prob=filter.for.niche.evolution.prob)
+    groups = x$groups
+    predictors = x$predictors
+    rv = analyze.dapc(predictors, groups, n.pc, n.da)
+    rv
 }
